ece43c76a2409ebe515e654bbcd5155334ca7af7
MK-241095 <maick.scp@gmail.com>
2026-05-17 20:37:40 -0600

    Nitro 150

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,8 +1,23 @@
-import { Box } from "@chakra-ui/react"
+import { Box,Heading,Image} from "@chakra-ui/react"
 
 export default function Category() {
+    
+    return <> 
 
-    return <>
-        <Box>Menú de categorías</Box>
-    </>
+    <Box backgroundColor={"red"} textAlign={"center"}> 
+
+<Heading size="2xl" color={"#00ff00"}> Fasti</Heading>
+             
+    </Box>
+
+    <Box backgroundColor={"skyblue"} textAlign={"center"}>
+       <Heading size="2xl" color={"#ffffff"}> <Image src="/image/NITRO 150.png"/>
+        </Heading> 
+       
+
+
+    </Box>
+    
+   </> 
+
 }
