ecfbff007215e384407a9014c8aa19e95d65fcc4
Dan-Lopez94 <craizy_x3@hotmail.com>
2026-06-10 21:58:43 -0600

    Card

--- src/Components/CardComponents/CategoryCard.js
+++ src/Components/CardComponents/CategoryCard.js
@@ -1,13 +1,16 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Heading, Image, Text } from "@chakra-ui/react"
 
 
 export default function CategoryCard() {
 
     return <>
-        <Box maxW={"250px"} maxH={"300px"} backgroundColor={"skyblue"} overflow={"hidden"}>
-            <Box h={"100%"}>
-                {Array.from({ length: 20 }).map((_, i) => (
-                    <Text key={i}>Contenido</Text>))}
+        <Box maxW={"250px"} h={"300px"} bgColor={"white"} borderRadius={10} border={"1px"}>
+            <Box h={"70%"} padding={"10px 0px"}>
+                <Image borderRadius={10} w={"90%"} h={"100%"} objectFit={"cover"} src="/image/transmission.jpeg" alt="not found" />
+            </Box>
+            <Box spacing="2" textAlign={"left"} pl={2}>
+                <Heading size={"md"}>Transmisión</Heading>
+                <Text>Se hace cambio de transmisión a una moto</Text>
             </Box>
         </Box>
     </>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,12 @@
 import { Box } from "@chakra-ui/react"
-/* import CategoryCard from "./CardComponents/CategoryCard" */
+import CategoryCard from "./CardComponents/CategoryCard"
 
 export default function Home() {
 
     return (<Box bgImage={"url('/image/font.jpg')"} bgRepeat={"no-repeat"} bgSize={"cover"}>
         <Box bgImage="url('/image/font2.jpeg')" bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"contain"} h={"100vh"}>
-            <Box align="center">
-                {/* <CategoryCard /> */}
+            <Box align="center" p={2}>
+                <CategoryCard />
             </Box>
         </Box>
     </Box>
