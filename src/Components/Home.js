bc01f4acb139a5d1cbffbca7a713671b41749298
Dan-Lopez94 <craizy_x3@hotmail.com>
2026-06-14 22:03:16 -0600

    newProps

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (<Box bgImage={"url('/image/font.jpg')"} bgRepeat={"no-repeat"} bgSize={"cover"}>
         <Box bgImage="url('/image/font2.jpeg')" bgPosition={"center"} bgRepeat={"no-repeat"} bgSize={"contain"} h={"100vh"}>
             <Box>
-                <Grid templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)', lg: 'repeat(3, 1fr)' }} gap={3} align="center">
+                <Grid templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)', lg: 'repeat(3, 1fr)' }} gap={2} align="center">
                     <GridItem colSpan={1}>
                         <CategoryCard
                             img="/image/transmission.jpeg"
